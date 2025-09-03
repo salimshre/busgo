a63305c612672548e9b728e9ccfd55fed4f66d2b
Salim Shrestha <salim9shrestha@gmail.com>
2025-09-03 18:22:29 +0545

    up

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>BusGo</h1>
+    <p>BusGo is a bus booking platform that allows users to book bus tickets online.</p>
+    <button>Book Now</button>
+    <button>Sign Up</button>
+    <button>Log In</button>
+</body>
+</html>
