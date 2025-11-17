e461a3a1da0c967820e667f105c884e63b45b238
Salim Shrestha <salim9shrestha@gmail.com>
2025-11-17 16:53:33 +0545

    update

--- bms/bms-frontend/src/components/shared/Header.jsx
+++ bms/bms-frontend/src/components/shared/Header.jsx
@@ -1,14 +1,15 @@
 import React from "react"
-
+import logosalim from '../../assets/main-icon.png';
 
 const Header = () => {
   return (
     <div className="w-full text-sm bg-white">
       <div className="px-4 md:px-8">
-        <div className="max-w-screen-x1 mx-auto flex justify-between items-center py-3">
+        <div className="max-w-screen-xl mx-auto flex justify-between items-center py-3">
             {/*left part*/}
             <div className="flex items-center space-x-4"> </div>
-            <img src="" alt="logo" className="h-8 object-contain cursor-pointer"/>
+            
+            <img src={logosalim} alt="logo" className="h-8 object-contain cursor-pointer"/>
 
         </div>
       </div>
--- bms/readme.txt
+++ bms/readme.txt
@@ -1,7 +1,7 @@
 https://www.youtube.com/watch?v=XA0Yd50Au_o&list=PL9OdiypqS7NlD84nIN-rAh9D-LtzNi10R&index=4
 
 #3 router
-time: 2:57
+time: 8:05
 
 PS C:\Users\ACER-WIN10\Documents\GitHub\busgo\bms\bms-frontend> npm run dev  
 
@@ -10,3 +10,5 @@ PS C:\Users\ACER-WIN10\Documents\GitHub\busgo\bms\bms-backend> npm run dev
 
 note:
 rafc //will generate an templates. 
+
+
